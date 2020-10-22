7c47670af650c3c064698ffb0bb0ff2f47be21c4
dhruv-varshney <53403223+dhruv-varshney@users.noreply.github.com>
2020-10-23 01:48:44 +0530

    Add files via upload

---
+++ prac.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.util.Scanner;
+class dhruv
+{
+public static void main(String[] args)
+{
+int rev=0,k,n,r;
+Scanner sc=new Scanner(System.in);
+System.out.println("\nenter your no.\n");
+n = sc.nextInt();
+k = n;
+if(n==0)
+{
+return ;
+}
+else
+{
+while(n!=0)
+{
+r = n%10;
+rev = rev*10 + r;
+n = n/10;
+}
+}
+if(k==rev)
+{
+System.out.println("\n "+ k +"is a palindrom and its reverse is"+rev+"\n");
+}
+else
+{
+System.out.println("\n "+ k +"is not a palindrom and its reverse is"+rev+"\n");
+}
+}
+}
+
